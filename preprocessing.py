6ae565d377459a3e8b9bcb4537c483ca0a04c264
jayantkashyap <jayant.kashyap1@gmail.com>
2018-05-19 18:49:24 -0700

    added dataset loading function

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,10 @@
-import os
-import sys
 import cv2
-import numpy as np
-import pandas as pd
 
 class Preprocessing:
-    def __init__(self):
-        pass
+    def __init__(self, height, width, interpolation=cv2.INTER_AREA):
+        self._height = height
+        self._width = width
+        self._interpolation = interpolation
 
-    def preprocess(self):
-        pass
+    def preprocess(self, image):
+        return cv2.resize(image, (self._height, self._width), interpolation=self._interpolation)
